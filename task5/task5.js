296ba75ba2cd4b06352ee71c03ef265cbb0cfac4
Emin <eminexoazeri@gmail.com>
2026-09-03 23:38:07 +0400

    Level 2 Task 3: CSS Frameworks implemented with Tailwind CSS

---
+++ task5/task5.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const alertBtn = document.getElementById("alertBtn");
+
+    if (alertBtn) {
+        alertBtn.addEventListener("click", () => {
+            alert("Patron, Codveda Level 2 Task 3 uğurla işləyir və interaktivdir!");
+        });
+    }
+
+    console.log("Codveda Frontend Task yükləndi və hazır vəziyyətdədir.");
+});
